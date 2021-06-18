2f05e9cefe370d084d6bd533d4f1a99398f2a78e
Jan-Kees van Andel <jan-kees@streammachine.io>
2021-06-18 11:03:42 +0200

    chore(apis): removed rtrietsch from path

--- api-linter.sh
+++ api-linter.sh
@@ -4,13 +4,13 @@ proto_files=$(find protos/io/streammachine/api -name '*.proto')
 
 echo $proto_files
 
-#/Users/rtrietsch/Downloads/api-linter --config api-linter.yaml -I protos $proto_files
+api-linter --config api-linter.yaml -I protos $proto_files
 
-for file in $proto_files; do
-  echo "------------------------------------------"
-  echo "$file"
-  echo "------------------------------------------"
-  /Users/rtrietsch/Downloads/api-linter --config api-linter.yaml -I protos "$file"
-  echo
-  echo
-done
+#for file in $proto_files; do
+#  echo "------------------------------------------"
+#  echo "$file"
+#  echo "------------------------------------------"
+#  api-linter --config api-linter.yaml -I protos "$file"
+#  echo
+#  echo
+#done
