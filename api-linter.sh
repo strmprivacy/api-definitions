7871b835a96cfedb42e0d4ac98f3a372b15f2973
Jan-Kees van Andel <jan-kees@streammachine.io>
2021-06-18 12:48:40 +0200

    feat(apis): fixed all AIP lint errors

--- api-linter.sh
+++ api-linter.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-proto_files=$(find protos/io/streammachine/api -name '*.proto')
+proto_files=$(find protos -name '*.proto')
 
 declare -i EXIT_STATUS_CODE=0
 
 for file in $proto_files; do
-  echo "------------------------------------------"
-  echo "$file"
-  echo "------------------------------------------"
   api-linter \
     --set-exit-status \
-    --config api-linter.yaml \
+    --config .api-linter.yaml \
     -I /included-protos/ \
     -I protos \
     "$file"
   EXIT_STATUS_CODE+=$?
-  echo
 done
 
-echo $EXIT_STATUS_CODE
+echo "API Linting done. Exit code: $EXIT_STATUS_CODE"
 exit $EXIT_STATUS_CODE
