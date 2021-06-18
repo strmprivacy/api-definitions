7948e84d3cebd9163ce507b6f6ad78dcd8954f70
Robin Trietsch <robin@streammachine.io>
2021-06-18 10:54:27 +0200

    feat: add api linter scripts and comply to Google AIP rules

--- api-linter.sh
+++ api-linter.sh
@@ -2,6 +2,10 @@
 
 proto_files=$(find protos/io/streammachine/api -name '*.proto')
 
+echo $proto_files
+
+#/Users/rtrietsch/Downloads/api-linter --config api-linter.yaml -I protos $proto_files
+
 for file in $proto_files; do
   echo "------------------------------------------"
   echo "$file"
