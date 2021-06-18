712cfa968d7ddbdf086ef48301c23de2d5f3be93
Robin Trietsch <robin@streammachine.io>
2021-06-18 17:48:18 +0000

    ci: re-enable buf breaking

--- buf-breaking.sh
+++ buf-breaking.sh
@@ -1,6 +1,22 @@
-#!/bin/bash
-export LATEST_TAG=$(git tag -l | sort -V | tail -n 1)
-if [ ! -z "$LATEST_TAG" ]; then
-  echo "running buf breaking against $LATEST_TAG"
-  buf breaking --against ".git#tag=$LATEST_TAG"
-fi
+#!/bin/sh
+
+# Unfortunately, when comparing with Git, Buf cannot resolve external proto's,
+# since these are not part of the repository. As far as I can see, it's impossible to
+# configure Git to use a local "protos root" directory when comparing with Git.
+# To bypass this limitation, we compare with a locally generated image,
+# which we generate here, based on the latest master revision.
+
+# Checkout the latest master revision in a subdirectory.
+git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/stream-machine/api-definitions.git prev-api-definitions
+
+cd prev-api-definitions
+
+# link to the google-api-linter directory, which is used as a "protos root".
+# This symlink is the real reason for this whole workaround
+ln -sf ../google-api-linter google-api-linter
+buf build -o image.bin
+
+cd ..
+
+# Finally, compare the current API against the previous master API.
+buf breaking --against prev-api-definitions/image.bin
