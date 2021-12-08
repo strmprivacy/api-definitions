ee379b021d8d4ad7ff987681e81c1d368f82a7cf
Robin Trietsch <robin@strmprivacy.io>
2021-12-08 15:10:19 +0100

    feat: split agent protos into multiple files; add kafka_exporters_agent; fix buf breaking script

--- scripts/buf-breaking.sh
+++ scripts/buf-breaking.sh
@@ -6,20 +6,30 @@
 # To bypass this limitation, we compare with a locally generated image,
 # which we generate here, based on the latest master revision.
 
+if [ -f "$CI_JOB_TOKEN" ]; then
+    CLONE_URL="https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/strmprivacy/api-definitions.git"
+    BUF_IMAGE_LOCATION="image.bin"
+    ENSURE_COMMON_PROTOS_COMMAND="ln -sf ../google-api-linter google-api-linter"
+  else
+    CLONE_URL="git@gitlab.com:strmprivacy/api-definitions.git"
+    BUF_IMAGE_LOCATION="prev-api-definitions/image.bin"
+    ENSURE_COMMON_PROTOS_COMMAND="cp -r ../lang/.common-protos lang/.common-protos"
+fi
+
 # Checkout the latest master revision in a subdirectory.
-git clone https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/strmprivacy/api-definitions.git prev-api-definitions
+git clone "$CLONE_URL" prev-api-definitions
 
 cd prev-api-definitions
 
 # link to the google-api-linter directory, which is used as a "protos root".
 # This symlink is the real reason for this whole workaround
-ln -sf ../google-api-linter google-api-linter
+eval "$ENSURE_COMMON_PROTOS_COMMAND"
 buf build -o image.bin
 
 cd ..
 
 # Finally, compare the current API against the previous master API.
-buf breaking --against prev-api-definitions/image.bin
+buf breaking --against "$BUF_IMAGE_LOCATION"
 exit_code=$?
 
 # Clean up generated files afterwards
