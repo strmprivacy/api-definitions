b3bcc6bdb60584d8e23e586a071e082474025052
Robin Trietsch <robin@strmprivacy.io>
2022-01-17 17:14:34 +0100

    chore: update catalog-info

--- scripts/generate-catalog-info-yamls.sh
+++ scripts/generate-catalog-info-yamls.sh
@@ -26,22 +26,24 @@ for file in $proto_files; do
   # Check if the file actually has a service
   if grep -q "service " "$file"; then
     api_type=$(echo "$file" | awk -F'/' '{ print $2 }')
-    api_section=$(echo "$file" | awk -F'/' '{ print $3 }')
+    api_component=$(echo "$file" | awk -F'/' '{ print $3 }')
     api_version=$(echo "$file" | awk -F'/' '{ print $4 }')
     proto_file_name=$(echo "$file" | awk -F'/' '{ print $NF }' | awk -F'.' '{ print $1 }')
     service_name=$(awk -F" " '$1=="service"{print $2;exit;}' "$file")
 
-    destination_file="$api_type-$api_version-$api_section-$proto_file_name.yaml"
+    destination_file="$api_type-$api_version-$api_component-$proto_file_name.yaml"
 
 cat << EOF > "$CATALOG_INFO_DIR_RELATIVE/$destination_file"
 apiVersion: backstage.io/v1alpha1
 kind: API
 metadata:
-  name: $api_section-$proto_file_name
+  name: $api_component-$api_type-$proto_file_name
   namespace: core
   title: "$service_name $api_version"
   labels:
     api-type: "$api_type"
+    api-version: "$api_version"
+    api-component: "$api_component"
 spec:
   type: grpc
   lifecycle: production
